24dd14ed98b5b10e8dcfd1c84eb5f064a40bc372
mnmustafa1109 <mnmustafa1109@gmail.com>
2022-10-14 08:55:54 +0500

    updated install dependency

--- install-dependency.sh
+++ install-dependency.sh
@@ -1,8 +1,8 @@
+sudo apt install git
 sudo apt install clang
 sudo apt install cmake
 sudo apt install xorg-dev
 sudo apt install udev
-apt-get install libudev-dev
 sudo apt-get install libudev-dev
 sudo apt-get install libopenal-dev
 sudo apt-get install libvorbis-dev
